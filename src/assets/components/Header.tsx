511cd5d45567d8720ad6c33c85678dee15253303
Erivaldo Cavalcante <cavalcanteerivaldo0@gmail.com>
2022-12-18 13:37:57 -0300

    fix bugs

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -21,7 +21,7 @@ function Header(){
                 <a href="" className="text-white font-semibold px-5 lg:px-10">Comprar NFT</a>
                 <a href="" className="text-white font-semibold px-5 lg:px-10">Sobre</a>
                 <a href="" className="text-white font-semibold px-5 lg:px-10">FAQ</a>
-                <a href="" className="flex items-center px-2 lg:px-4 text-sm 32 lg:text-base py-2 bg-white font-bold md:ml-10"><img src={Mask} className="w-6 mr-1"/>Conectar carteira</a>
+                <a href="" className="flex items-center px-2 lg:px-4 text-sm lg:text-base py-2 bg-white font-semibold sm:font-bold md:ml-10"><img src={Mask} className="w-6 mr-1"/>Conectar carteira</a>
             </div>
             </nav>
         </div>
